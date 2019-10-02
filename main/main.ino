bfd24576883e1a821ab01f79b0b1ef29ef2d590e
Lyokolux <lyokolux@protonmail.com>
2019-10-02 14:20:07 +0200

    :bento: Add constants header

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,10 @@
 #include <Servo.h>
+#include "constants.h"
 
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
